249c5a226662eb1542b60451b7cd9707e09cbc5a
ChoiHyeonSeong <huns1013@gmail.com>
2022-09-14 01:37:03 +0900

    code

--- main.py
+++ main.py
@@ -23,14 +23,15 @@ train_reshape
 ok_lab = [0, 1, 2, 3, 4, 6, 7, 8, 9]
 i=1	# 파일명을 다르게 지정하기 위한 변수
 for img, lab in zip(train_reshape, label):
-  if lab in ok_lab:
-    # 256x256 resize, 보간법 적용
-    img_resize = cv2.resize(img, dsize=(256,256), interpolation=cv2.INTER_CUBIC)
-    # 선명하게 만들기
-    kernel = np.array([[0, -1, 0],
+    if lab in ok_lab:
+        # 256x256 resize, 보간법 적용
+        img_resize = cv2.resize(img, dsize=(256,256), interpolation=cv2.INTER_CUBIC)
+        # 선명하게 만들기
+        kernel = np.array([[0, -1, 0],
                       [-1, 5, -1],
                       [0, -1, 0]])
-    img_sharp = cv2.filter2D(img_resize, -1, kernel)
+        img_sharp = cv2.filter2D(img_resize, -1, kernel)
     # 이미지 파일 생성
-    cv2.imwrite('data/'+str(lab) + '_' + str(i)+'.jpg', img_sharp)	# 파일명 ex : 0_1.jpg
-  i += 1
+        cv2.imwrite('data/'+str(lab) + '_' + str(i)+'.jpg', img_sharp)	# 파일명 ex : 0_1.jpg
+    i += 1
+
