25706f966e84c96f406edd7ddc81a07856ace0fd
ChoiHyeonSeong <huns1013@gmail.com>
2022-09-13 14:30:33 +0900

    _1

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn, optim
+from torch.utils.data import (Dataset, DataLoader, TensorDataset)
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data = pd.read_csv('C:/Users/huns1/OneDrive/바탕 화면/Pycharm/Dataset/Image_1/train.csv')
+train = data.iloc[:,2:]
+label = data.iloc[:,1:2]
+
+train = np.array(train,dtype='uint8')
+label = np.array(label)
+train.shape
+
+train_reshape = train.reshape(60000, 28, 28)
+train_reshape.shape
+train_reshape
+
+ok_lab = [0, 1, 2, 3, 4, 6, 7, 8, 9]
+i=1	# 파일명을 다르게 지정하기 위한 변수
+for img, lab in zip(train_reshape, label):
+  if lab in ok_lab:
+    # 256x256 resize, 보간법 적용
+    img_resize = cv2.resize(img, dsize=(256,256), interpolation=cv2.INTER_CUBIC)
+    # 선명하게 만들기
+    kernel = np.array([[0, -1, 0],
+                      [-1, 5, -1],
+                      [0, -1, 0]])
+    img_sharp = cv2.filter2D(img_resize, -1, kernel)
+    # 이미지 파일 생성
+    cv2.imwrite('C:/Users/huns1/OneDrive/바탕 화면/Pycharm/Dataset/'+str(lab) + '_' + str(i)+'.jpg', img_sharp)	# 파일명 ex : 0_1.jpg
+  i += 1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+batch_size = 128
+train_loader = DataLoader(fashion_mnist_train,
+                          batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(fashion_mnist_train,
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+                         batch_size=batch_size, shuffle=False)
