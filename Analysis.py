b2b16d54c0f1ee1e08b2b7365d2c96788d5e06f3
ChoiHyeonSeong <huns1013@gmail.com>
2022-09-14 10:14:27 +0900

    _1

--- Analysis.py
+++ Analysis.py
@@ -1,16 +1,38 @@
+# import package
+
+# model
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.init as init
+import torch.nn.functional as F
+from torch import optim
+from torch.optim.lr_scheduler import StepLR
 
-import torchvision
-import torchvision.datasets as datasets
+# dataset and transformation
+from torchvision import datasets
 import torchvision.transforms as transforms
-
 from torch.utils.data import DataLoader
+import os
 
-import numpy as np
+# display images
+from torchvision import utils
 import matplotlib.pyplot as plt
 
-import tqdm
-from tqdm.auto import trange
+# utils
+import numpy as np
+import time
+import copy
+
+# specify the data path
+path2data = '/data'
+
+# if not exists the path, make the directory
+if not os.path.exists(path2data):
+    os.mkdir(path2data)
+
+# load dataset
+train_ds = datasets.STL10(path2data, split='train', download=True, transform=transforms.ToTensor())
+val_ds = datasets.STL10(path2data, split='test', download=True, transform=transforms.ToTensor())
+
+print(len(train_ds))
+print(len(val_ds))
+
